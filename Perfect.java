7e0d83e80e5c49a0a193be80e28c54a73eed9987
Haile <haile.tasma@runi.ac.il>
2024-01-01 11:14:38 +0200

    try 7

--- Perfect.java
+++ Perfect.java
@@ -6,7 +6,7 @@ public class Perfect {
 		String helper =" ";
 		String ans = "";
 		int counter=0;
-		int TheNumIsPerfect =6;// Integer.parseInt(args[0]);
+		int TheNumIsPerfect =Integer.parseInt(args[0]);
 		for (int i = 1; i < TheNumIsPerfect ; i++) {
 			if (TheNumIsPerfect%i==0) {
 				counter+=i;
