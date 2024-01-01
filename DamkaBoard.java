6f14167f58ea0e16ab4a191fba5c9a02267c8e88
Haile <haile.tasma@runi.ac.il>
2024-01-01 17:45:55 +0200

    try 25

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,14 +3,16 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		int lines= 4;//Integer.parseInt(args[0]);
+		int lines= 3;//Integer.parseInt(args[0]);
 		for (int i = 0; i < lines; i++) {
 			for (int j = 0; j < lines; j++) {
 				if(lines-1==j){
-				System.out.print("*"+"\n");
+				System.out.print("*");
 				}else
 				System.out.print("* ");
-			}		
+			}
+			if(i!=lines-1)
+			System.out.println();		
 			if(i%2==0){   /// Helps to make space between lines 
 			System.out.print(" "); 
 			}	
